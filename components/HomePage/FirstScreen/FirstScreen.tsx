adafcd23ae3fce57316443cd4c2a33624aa76ca6
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-09-09 21:41:43 +0700

    fix: сдвинуть текст левее

--- components/HomePage/FirstScreen/FirstScreen.tsx
+++ components/HomePage/FirstScreen/FirstScreen.tsx
@@ -6,7 +6,7 @@ export default function FirstScreen() {
       className={`bg-[url("/firstScreen.png")] bg-blend-overlay bg-black bg-opacity-40 flex items-center w-full h-screen bg-center bg-no-repeat bg-fixed bg-cover justify-center`}
     >
       <div className=" text-white px-3 md:w-1/2 md:p-0">
-        <p className="font-bold text-xl mb-5 md:text-3xl">
+        <p className="font-bold text-xl mb-5 md:text-2xl">
           Аудиторская группа
           <br className="block sm:hidden"/>
            {'"Аземша и партнеры"'}
@@ -15,8 +15,9 @@ export default function FirstScreen() {
         <p className="font-medium text-base mb-7 md:text-xl">
           Приносим реальную пользу своим клиентам!
         </p>
-        <button className="bg-white  font-bold uppercase tracking-wider rounded-sm text-black px-8 py-2 hover:bg-accent-color duration-300 active:scale-95">Рассчитать</button>
+        <button className="bg-white outline-none font-bold uppercase tracking-wider rounded-sm text-black px-8 py-2 hover:bg-accent-color duration-300 active:scale-95">Рассчитать</button>
       </div>
+        <div className="hidden w-1/6 md:block"></div>
     </section>
   );
 }
