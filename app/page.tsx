e0daf3b0917d94c2f732bc49784a5b077eb0bf48
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-09-09 21:45:18 +0700

    feat: добавить компонент \"О нас\" на главную страницу

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutUs from "@/components/HomePage/AboutUs/AboutUs";
 import FirstScreen from "@/components/HomePage/FirstScreen/FirstScreen";
 
 export default function HomePage() {
   return (
     <main>
       <FirstScreen/>
+      <AboutUs/>
     </main>
   )
 }
