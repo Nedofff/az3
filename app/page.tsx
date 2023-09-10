daa0a11d291ba046bc9da3894d0571212bda31cf
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-09-10 18:19:08 +0700

    feat: добавить блок services - услуги

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutUs from "@/components/HomePage/AboutUs/AboutUs";
 import FirstScreen from "@/components/HomePage/FirstScreen/FirstScreen";
+import Services from "@/components/Services/Services";
 
 export default function HomePage() {
   return (
     <main>
       <FirstScreen/>
       <AboutUs/>
+      <Services/>
     </main>
   )
 }
