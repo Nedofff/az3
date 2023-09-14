49175e2ef602f6a6daf2bf148ad8b2766535f85f
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-09-14 15:54:33 +0700

    feat: добавить apikey для яндекс карт

--- components/HomePage/YandexMap/YandexMap.tsx
+++ components/HomePage/YandexMap/YandexMap.tsx
@@ -5,7 +5,7 @@ import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
 export default function YandexMap() {
   return (
-    <YMaps>
+    <YMaps query={{ lang: 'ru_RU', apikey:'254603c8-4a91-49d1-b86d-604acd276688' }}>
       <Map className='w-full h-80' defaultState={{ center: [56.012086852244444, 92.89065258388192], zoom: 16 }} >
       <Placemark geometry={[56.012086852244444, 92.89065258388192]} />
       </Map>
