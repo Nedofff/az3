2019829ed3e8cc1e40277f354ec442f265f08ad2
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-10-11 14:47:22 +0700

    feat: переписать компонент модально окна
    Разделить на два окна
    1 окно является базовым для сообщений
    2 окно открывает картинки

--- components/BaseModals/DialogBaseModal.tsx
+++ components/BaseModals/DialogBaseModal.tsx
@@ -1,15 +1,15 @@
 'use client'
 import React from "react";
 import {createPortal} from "react-dom";
-import styles from './Modal.module.css'
+import styles from './DialogModal.module.css'
 
 const ModalWindow = ({children, title, onClose }:{children:React.ReactNode, title:React.ReactNode, onClose:()=>void}) => {
 
   return (
-    <div onClick={onClose} role="dialog" className="w-screen sm:p-5 p-0 h-screen fixed top-0 bg-opacity-70 flex items-center justify-center left-0 z-50 bg-black">
-      <div onClick={e => e.stopPropagation()} className="bg-white py-5 rounded-sm px-10 h-screen w-screen sm:h-auto sm:w-auto">
+    <div onClick={onClose} role="dialog" className="w-screen sm:p-5 p-0 h-screen fixed overflow-y-scroll top-0 bg-opacity-70 flex items-center justify-center left-0 z-50 bg-black">
+      <div onClick={e => e.stopPropagation()} className="bg-white py-5 rounded-sm px-10 h-screen w-screen sm:h-auto sm:w-3/4">
         <div className="flex justify-between pb-2 space-x-4">
-        <>{title}</>
+        <h3>{title}</h3>
         <button onClick={onClose} className={styles.close}><span></span></button>
         </div>
         <div>
@@ -21,7 +21,7 @@ const ModalWindow = ({children, title, onClose }:{children:React.ReactNode, titl
 };
 
 
-const BaseModal = ({children, title, onClose}:{children:React.ReactNode, title:any, onClose:()=>void}) => {
+const DialogBaseModal = ({children, title, onClose}:{children:React.ReactNode, title:any, onClose:()=>void}) => {
   return (
     <>
     {createPortal(<ModalWindow onClose={onClose} title={title}>{children}</ModalWindow>, document.body)}
@@ -29,4 +29,4 @@ const BaseModal = ({children, title, onClose}:{children:React.ReactNode, title:a
   );
 };
 
-export default BaseModal;
+export default DialogBaseModal;
---
+++ components/BaseModals/ImgScaleBaseModal.tsx
@@ -0,0 +1,82 @@
+"use client";
+import React, { useLayoutEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+import styles from "./ImgScaleBaseModal.module.css";
+import Image from "next/image";
+
+const ModalWindow = ({
+  src,
+  width,
+  height,
+  alt,
+  onClose,
+}: {
+  onClose: () => void;
+  src: string;
+  width: number;
+  height: number;
+  alt: string;
+}) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const clickHandler = () => {
+    ref.current!.classList.add('fade-out-div')
+    setTimeout(() => {
+      onClose();
+    }, 290)
+  }
+
+  return (
+    <>
+      <div
+        onClick={clickHandler}
+        ref={ref}
+        role="dialog"
+        className="w-full fade-in-div h-full fixed top-0 bg-opacity-70 left-0 flex items-center justify-center z-50 bg-black"
+      >
+        <div className='relative w-full h-full flex items-center justify-center'>
+          <button onClick={clickHandler} className={styles.close}>
+            <span></span>
+          </button>
+          <Image
+            className="w-auto h-4/5 cursor-zoom-out"
+            src={src}
+            width={width}
+            height={height}
+            alt={alt}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const ImgScaleBaseModal = ({
+  src,
+  width,
+  height,
+  alt,
+  onClose,
+}: {
+  onClose: () => void;
+  src: string;
+  width: number;
+  height: number;
+  alt: string;
+}) => {
+  return (
+    <>
+      {createPortal(
+        <ModalWindow
+          onClose={onClose}
+          src={src}
+          width={width}
+          height={height}
+          alt={alt}
+        />,
+        document.body
+      )}
+    </>
+  );
+};
+
+export default ImgScaleBaseModal;
