d3cb26873ee67f1b2e448d0a30ac4f933048843c
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-10-11 15:32:30 +0700

    feat: реализовать компонент выпадающего листа

--- components/Dropdown/Dropdown.tsx
+++ components/Dropdown/Dropdown.tsx
@@ -5,9 +5,11 @@ interface IProps {
   label: string;
   children: React.ReactNode;
   className?: string
+  mobileBoard?: boolean
+  needHeading?: boolean
 }
 
-export default function Dropdown({ children, label, className }: IProps) {
+export default function Dropdown({ children, label, className, mobileBoard = true, needHeading = false }: IProps) {
   const [isOpen, setIsOpen] = useState(false);
   const refCollapsed = useRef<HTMLDivElement>(null);
   const toggle = useCallback(() => {
@@ -21,9 +23,12 @@ export default function Dropdown({ children, label, className }: IProps) {
 
   return (
       <div className={className}>
-        <button className=" text-white outline-none duration-150 w-full py-2 bg-sub-color hover:bg-accent-color" onClick={toggle}>{label}</button>
+        <button className=" text-white outline-none duration-150 w-full py-2 bg-sub-color hover:bg-accent-color" onClick={toggle}>
+          {needHeading && <h2 className='text-xl text-sub-color font-semibold mb-2'>{label}</h2>}
+          
+        </button>
         <div className='duration-300 overflow-hidden max-h-0' ref={refCollapsed}>
-          <div className='py-3 border-sub-color border-r-2 border-l-2 mb-2'> {children} </div>
+          <div className={`py-3 border-sub-color ${mobileBoard ? 'border-r-2 border-l-2 mb-2' : 'sm:border-r-2 sm:border-l-2 mb-2'}`}> {children} </div>
         </div>
       </div>
   );
