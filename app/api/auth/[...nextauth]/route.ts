dadb674d7a248fb514e38c7e33a7b005d1e046cf
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-10-17 13:15:13 +0700

    feat: подклчить next-auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth"
+import type { AuthOptions, User } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+
+const users: {id:string, name: string; password: string}[] = [
+  {id:'1',  name: "1", password: "1"},
+];
+
+
+export const authConfig: AuthOptions = {
+  providers: [
+    Credentials({
+      credentials: {
+        name: { label: "login", type: "login" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.name || !credentials?.password) return null;
+
+        const currentUser = users.find((user) => user.name === credentials.name);
+
+        if (currentUser && currentUser.password === credentials.password) {
+          const { password, ...userWithoutPass } = currentUser;
+
+          return userWithoutPass as User;
+        }
+
+        return null
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/admin/auth'
+  }
+};
+
+
+const handler = NextAuth(authConfig)
+
+export { handler as GET, handler as POST }
+
