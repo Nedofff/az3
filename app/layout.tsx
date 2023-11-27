81d05dd44a5be2f9df80ea0e56bdf94caaa0fea2
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-11-27 18:04:49 +0700

    fix: исправить малый экран

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className="overflow-x-hidden" lang="ru">
+    <html className="overflow-x-hidden w-screen" lang="ru">
       <body>
         <StrictMode>
         <Providers>
