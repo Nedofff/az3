ccf3879d1ec98d7812224de9b59a88ca4fb16c2e
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-12-02 20:22:09 +0700

    fix: еще один финальный штрих

--- app/admin/(panel)/news/edit/page.tsx
+++ app/admin/(panel)/news/edit/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
         <p className='text-white text-lg font-normal text-center'>Нажмите на новость для редактирования</p>
         <Link className='bg-main-color mr-1 px-5 py-2 text-black rounded-md hover:bg-opacity-50 duration-200 text-center' href={'/admin/news'}>Назад</Link>
         </div>
-        <div className='space-y-2 max-h-[450px] overflow-auto bg-slate-200'>
+        <div className='space-y-2 max-h-[450px] overflow-auto py-2'>
         {Boolean(news) && news!.map(item => (
             <OneNewsForEdit onClick={() => deleteHadnelr(item.id)} key={item.id} id={item.id} title={item.title}/>
         ))}
--- components/AdminPage/OneNewsForEdit/OneNewsForEdit.tsx
+++ components/AdminPage/OneNewsForEdit/OneNewsForEdit.tsx
@@ -10,9 +10,9 @@ export default function OneNewsForEdit({id, title, onClick}:{id:string, title:st
       onClick()
     }
   return (
-    <div className='flex '>
+    <div className='flex'>
         <Link href={`/admin/news/edit/${id}/`} className='word-break block w-5/6 py-3 rounded-s-lg px-5 shadow-lg duration-500 hover:bg-sub-accent-color outline-none hover:text-white bg-white'>{title}</Link>
-        <button onClick={clickHandler} className='flex border-s-2 h-full text-2xl w-1/6 border-black py-3 rounded-e-lg sm:px-5 sm:pr-6 justify-center duration-500 shadow-lg bg-white hover:bg-red-600 outline-none hover:text-white'><BiSolidTrash/></button>
+        <button onClick={clickHandler} className='flex border-s-2 text-center items-center text-2xl w-1/6 border-black py-3 rounded-e-lg sm:px-5 sm:pr-6 justify-center duration-500 shadow-lg bg-white hover:bg-red-600 outline-none hover:text-white'><BiSolidTrash/></button>
     </div>
   )
 }
