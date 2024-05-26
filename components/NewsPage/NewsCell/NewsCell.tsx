fc7f6896c6318fdf8d05f87888cb9eaec2551653
Eduard Novikov <edyard.novikov.d@gmail.com>
2024-05-27 03:05:43 +0700

    fix: исправить стили изображения в ячейке новости

--- components/NewsPage/NewsCell/NewsCell.tsx
+++ components/NewsPage/NewsCell/NewsCell.tsx
@@ -34,7 +34,7 @@ export default function NewsCell({
     <div className="h-full">
     <Link href={`/news/${id}`} className='flex flex-col h-full mx-5 max-w-sm w-min-[450px] shadow-black cursor-pointer mb-7 bg-white shadow-lg rounded-lg md:m-3 duration-500 hover:duration-500 hover:shadow-2xl hover:shadow-black'>
       {Boolean(src) ? <div className='relative w-full h-2/3 overflow-hidden'>
-        <img src={getUrlImg(src)} alt={title} />
+        <img src={getUrlImg(src)} alt={title} className="w-full rounded-lg h-full object-center object-cover"/>
         <WaveSvg className="absolute inline bottom-0 fill-white " />
       </div> : <div></div>}
       <div className="relative px-5">
