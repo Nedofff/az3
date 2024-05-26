438c24557b886fe7bcb9eee03badbc66527088ef
Eduard Novikov <edyard.novikov.d@gmail.com>
2024-05-26 19:00:31 +0700

    feat: добавить правильную сортировку

--- app/api/admin/news/[id]/route.ts
+++ app/api/admin/news/[id]/route.ts
@@ -1,65 +1,74 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { join } from "path";
 import { writeFile } from "fs/promises";
-import sizeOf from 'image-size'
-import {exec} from 'node:child_process'
+import sizeOf from "image-size";
+import { exec } from "node:child_process";
 
-export async function DELETE(req:Request, { params }: { params: { id: string } }) {
-    const resultBD = await prisma.news.delete({
-        where: {
-            id: params.id
-        }
-    })
-    return NextResponse.json(resultBD)
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const resultBD = await prisma.news.delete({
+    where: {
+      id: params.id,
+    },
+  });
+  return NextResponse.json(resultBD);
 }
-export async function PUT(req:Request, { params }: { params: { id: string } }) {
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const formData = await req.formData();
+  const file: File | null = formData.get("file") as unknown as File;
 
-    const formData = await req.formData()
-    const file: File | null = formData.get("file") as unknown as File;
+  if (file) {
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-    if (file) {
-        const bytes = await file.arrayBuffer();
-        const buffer = Buffer.from(bytes);
-        
-        const nameFileFromSrc = (formData.get('srcToImage')! as string).split('/')[2]
-const nameFile = nameFileFromSrc ? `${nameFileFromSrc.split('.')[0]}${file.type.split("/")[1]}` : `${Date.now()}${file.type.split("/")[1]}`
-        const path = join(process.cwd(), "public/news", nameFile);
-        await writeFile(path, buffer);
+    const nameFileFromSrc = (formData.get("srcToImage")! as string).split(
+      "/"
+    )[2];
+    const nameFile = nameFileFromSrc
+      ? `${nameFileFromSrc.split(".")[0]}${file.type.split("/")[1]}`
+      : `${Date.now()}${file.type.split("/")[1]}`;
+    const path = join(process.cwd(), "public/news", nameFile);
+    await writeFile(path, buffer);
 
-        const dimensions = sizeOf(path);
+    const dimensions = sizeOf(path);
 
-        const width = dimensions.width!
-        const height = dimensions.height!
+    const width = dimensions.width!;
+    const height = dimensions.height!;
 
-        const resultBD = await prisma.news.update({
-            where: {
-                id: params.id
-            },
-            data: {
-                title: formData.get('header') as string,
-                srcToImage: `/news/${nameFile}`,
-                widthImg: width,
-                heightImg: height,
-                content: formData.get('html') as string,
-            }
-        })
+    const resultBD = await prisma.news.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        title: formData.get("header") as string,
+        srcToImage: `/news/${nameFile}`,
+        widthImg: width,
+        heightImg: height,
+        content: formData.get("html") as string,
+      },
+    });
 
-const restart = () => {exec('pm2 restart next')}
-    exec('cd /root/azmesha-and-partners-v2/ && npm run build', restart)
-    return NextResponse.json(resultBD)
-
-    } else {
-        const resultBD = await prisma.news.update({
-            where: {
-                id: params.id
-            },
-            data: {
-                title: formData.get('header') as string,
-                content: formData.get('html') as string,
-            }
-        })
-    return NextResponse.json(resultBD)
-    }
+    const restart = () => {
+      exec("pm2 restart next");
+    };
+    exec("cd /root/azmesha-and-partners-v2/ && npm run build", restart);
+    return NextResponse.json(resultBD);
+  } else {
+    const resultBD = await prisma.news.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        title: formData.get("header") as string,
+        content: formData.get("html") as string,
+      },
+    });
+    return NextResponse.json(resultBD);
+  }
 }
-
--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,68 +1,68 @@
-
-
-import { NextResponse } from "next/server"
-import prisma from "@/lib/prisma"
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const q = searchParams.get("q");
+  const page = searchParams.get("page");
 
-    const {searchParams} = new URL(req.url)
-    const q = searchParams.get('q')
-    const page = searchParams.get('page')
-    
-    if (!!q) {
-      const containsParam = String(q).split(' ').join(' & ')
-      const resultBD = await prisma.news.findMany({
-        select:{
-          id: true,
-          title: true,
-          srcToImage: true,
-          widthImg: true,
-          heightImg: true,
-        },
-          where: {
-              OR: [
-                {
-                  title: {
-                    search: containsParam,
-                  },
-                },
-                {
-                  content: {
-                    search: containsParam,
-                  },
-                },
-                {
-                tags: {
-                  hasSome: containsParam.split(' & '),
-              },}
-              ],
+  if (!!q) {
+    const containsParam = String(q).split(" ").join(" & ");
+    const resultBD = await prisma.news.findMany({
+      select: {
+        id: true,
+        title: true,
+        srcToImage: true,
+        widthImg: true,
+        heightImg: true,
+      },
+      where: {
+        OR: [
+          {
+            title: {
+              search: containsParam,
+            },
           },
-          orderBy: {
-            date: "desc",
+          {
+            content: {
+              search: containsParam,
+            },
           },
-        });
-        const result = resultBD.map(item => ({
-          id: item.id,
-          title: item.title,
-          src: item.srcToImage,
-          width: item.widthImg,
-          height: item.heightImg,
-        }))
-  
-      return NextResponse.json(result)
-    } else {
-      const resultBD = await prisma.news.findMany({
-        skip: Number(page) === 1 ? 0 : (Number(page) - 1) * 24,
-        take: 24
-      })
-      const result = resultBD.map(item => ({
-        id: item.id,
-        title: item.title,
-        src: item.srcToImage,
-        width: item.widthImg,
-        height: item.heightImg,
-      }))
-      return NextResponse.json(result)
-    }
-    
+          {
+            tags: {
+              hasSome: containsParam.split(" & "),
+            },
+          },
+        ],
+      },
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const result = resultBD.map((item) => ({
+      id: item.id,
+      title: item.title,
+      src: item.srcToImage,
+      width: item.widthImg,
+      height: item.heightImg,
+    }));
+
+    return NextResponse.json(result);
+  } else {
+    const resultBD = await prisma.news.findMany({
+      skip: Number(page) === 1 ? 0 : (Number(page) - 1) * 24,
+      take: 24,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const result = resultBD.map((item) => ({
+      id: item.id,
+      title: item.title,
+      src: item.srcToImage,
+      width: item.widthImg,
+      height: item.heightImg,
+    }));
+    return NextResponse.json(result);
+  }
 }
