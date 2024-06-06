0da5899072798e50eed7910e787fb71fec973169
Eduard Novikov <edyard.novikov.d@gmail.com>
2024-06-06 16:52:08 +0700

    Фикс новостей

--- app/(main)/news/[id]/page.tsx
+++ app/(main)/news/[id]/page.tsx
@@ -12,14 +12,16 @@ interface IProps {
   };
 }
 
-export async function generateMetadata(
-  { params }: IProps,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
+export async function generateMetadata({ params }: IProps): Promise<Metadata> {
   const id = params.id;
 
   const response = await fetch(`${env.NEXTAUTH_URL}/api/news/${id}`);
-  const {title} = await response.json();
+  let title;
+  if (!response.ok) {
+    title = "Новости";
+  }
+  const data = await response.json();
+  title = data.title;
 
   return {
     title,
@@ -35,48 +37,52 @@ interface IOneNews {
   text: string;
 }
 
-
 export default async function Page({ params }: IProps) {
   const response = await fetch(`${env.NEXTAUTH_URL}/api/news/${params.id}`, {
-    cache: 'no-store'
+    cache: "no-store",
   });
 
   if (response.ok) {
-  const newsData:IOneNews = await response.json();
-  if (Object.keys(newsData).length == 0) {
-    redirect('/404')
-}
-  const {title, text, src, ...forImg} = newsData
-  
+    const newsData: IOneNews = JSON.parse(await response.json());
+    if (Object.keys(newsData).length == 0) {
+      redirect("/404");
+    }
+    const { title, text, src, ...forImg } = newsData;
+
     return (
       <main className="pb-24 flex flex-col items-center bg-main-color">
-       {Boolean(src) && <section
-      className={` bg-blend-overlay relative bg-black bg-opacity-40 flex items-center w-full h-screen bg-center bg-no-repeat bg-cover justify-center`}
-    >
-       <OptimizedBgImg isNeedDark imageProps={{
-        src: getUrlImg(src),
-        ...forImg
-       }}/>
-      <div className=" text-white px-3 md:w-1/2 md:p-0 z-[1]">
-        <h1 className="font-bold text-lg sm:text-3xl uppercase md:text-6xl md:mb-2">{title}</h1>
-      </div>
-        <div className="hidden w-1/6 md:block"></div>
-    </section>}
-    
-    <section className="separator-min px-7 w-full flex flex-col items-center">
-    {
-      !Boolean(src) && <div className="pt-28">
-        <h1 className="heading">{title}</h1>
-      </div>
-    }
+        {Boolean(src) && (
+          <section
+            className={` bg-blend-overlay relative bg-black bg-opacity-40 flex items-center w-full h-screen bg-center bg-no-repeat bg-cover justify-center`}
+          >
+            <OptimizedBgImg
+              isNeedDark
+              imageProps={{
+                src: getUrlImg(src),
+                ...forImg,
+              }}
+            />
+            <div className=" text-white px-3 md:w-1/2 md:p-0 z-[1]">
+              <h1 className="font-bold text-lg sm:text-3xl uppercase md:text-6xl md:mb-2">
+                {title}
+              </h1>
+            </div>
+            <div className="hidden w-1/6 md:block"></div>
+          </section>
+        )}
 
-      <ParsingBlock text={text}/>
-    </section>
+        <section className="separator-min px-7 w-full flex flex-col items-center">
+          {!Boolean(src) && (
+            <div className="pt-28">
+              <h1 className="heading">{title}</h1>
+            </div>
+          )}
+
+          <ParsingBlock text={text} />
+        </section>
       </main>
     );
   } else {
-    redirect('/404')
+    redirect("/404");
   }
-
-  
 }
--- app/api/news/[id]/route.ts
+++ app/api/news/[id]/route.ts
@@ -1,24 +1,23 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const resultBD = await prisma.news.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  const result = {
+    id: resultBD?.id,
+    title: resultBD?.title,
+    src: resultBD?.srcToImage,
+    width: resultBD?.widthImg,
+    height: resultBD?.heightImg,
+    text: resultBD?.content,
+  };
 
-
-import { NextResponse } from "next/server"
-import prisma from "@/lib/prisma"
-
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-
-    const resultBD = await prisma.news.findUnique({
-        where: {
-            id: params.id
-        }
-    })
-    const result = {
-        id: resultBD?.id,
-        title: resultBD?.title,
-        src: resultBD?.srcToImage,
-        width: resultBD?.widthImg,
-        height: resultBD?.heightImg,
-        text: resultBD?.content,
-    }
-
-    return NextResponse.json(result)
+  return NextResponse.json(JSON.stringify(result));
 }
