e3f2442a1e0acaca40379b27ee36f1bea9d09ca1
Eduard Novikov <edyard.novikov.d@gmail.com>
2024-06-07 00:08:52 +0700

    Правки 249

--- app/admin/(panel)/news/edit/[id]/page.tsx
+++ app/admin/(panel)/news/edit/[id]/page.tsx
@@ -1,29 +1,50 @@
-import TextEditorBox from '@/components/TextEditor/TextEditorBox/TextEditorBox'
-import TextEditorProvider from '@/components/TextEditor/TextEditorProvider'
-import { News } from '@prisma/client'
-import React from 'react'
+import TextEditorBox from "@/components/TextEditor/TextEditorBox/TextEditorBox";
+import TextEditorProvider from "@/components/TextEditor/TextEditorProvider";
+import React from "react";
 
 interface IParams {
-    params: {
-        id: string
-    }
+  params: {
+    id: string;
+  };
 }
-export default async function Page({params}:IParams) {
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/admin/news?newsId=${params.id}`,
-  {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-cache:'no-store'
-  })
-  const data = await response.json() as News
-  const preinstalledContent = data.content
+
+type News = {
+  id: string;
+  title: string;
+  srcToImage: string | null;
+  date: Date;
+  widthImg: number | null;
+  heightImg: number | null;
+  content: string;
+  tags: string[];
+};
+
+export default async function Page({ params }: IParams) {
+  const response = await fetch(
+    `${process.env.NEXTAUTH_URL}/api/admin/news?newsId=${params.id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+    }
+  );
+  const data = (await response.json()) as News;
+  const preinstalledContent = data.content;
   return (
     <>
-    <TextEditorProvider preinstalledContent={preinstalledContent}>
-    {!!preinstalledContent ? <TextEditorBox newsId={params.id} title={data.title} srcToImage={data.srcToImage}/> : <div>Такого элемента не существует</div>}
-    </TextEditorProvider>
+      <TextEditorProvider preinstalledContent={preinstalledContent}>
+        {!!preinstalledContent ? (
+          <TextEditorBox
+            newsId={params.id}
+            title={data.title}
+            srcToImage={data.srcToImage}
+          />
+        ) : (
+          <div>Такого элемента не существует</div>
+        )}
+      </TextEditorProvider>
     </>
-  )
+  );
 }
