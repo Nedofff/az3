363bf904728df4daf00718f7368abc2664a168f5
Eduard Novikov <edyard.novikov.d@gmail.com>
2024-09-03 03:27:49 +0700

    03092024_0327

--- service/getUrlImg.ts
+++ service/getUrlImg.ts
@@ -1,6 +1,5 @@
 import { PATH_TO_IMAGES_WEB } from "@/consts/paths";
 
 export function getUrlImg(addingUrl: string) {
-  return addingUrl;
   return (PATH_TO_IMAGES_WEB + addingUrl).replaceAll(" ", "");
 }
